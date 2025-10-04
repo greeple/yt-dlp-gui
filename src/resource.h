a9089a8e72753683d5e2931abcb041f73c4dc642
greeple <autobot3@rambler.ru>
2025-10-04 13:01:16 +0300

    Update resource.h

--- src/resource.h
+++ src/resource.h
@@ -1,7 +1,8 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-#define IDC_DOWNLOAD 1001
-#define IDC_BROWSE   1002
+#define IDC_DOWNLOAD    1001
+#define IDC_BROWSE      1002
+#define IDC_TOGGLE_LOG  1003
 
 #endif
