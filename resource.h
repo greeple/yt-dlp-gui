fb1484bfcb7faaf5285bc8a4baec80ee6ea11a7c
greeple <autobot3@rambler.ru>
2025-10-05 19:26:31 +0300

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,10 @@
+#define IDC_DOWNLOAD_BUTTON 101
+#define IDC_TOGGLE_OUTPUT_BUTTON 102
+#define IDC_BROWSE_BUTTON 103
+#define IDC_TAB_CONTROL 104
+#define IDC_STATUS_BAR 105
+
+// Идентификаторы для элементов на вкладках
+#define IDC_STATIC_PATH 201
+#define IDC_OUTPUT_PATH_EDIT 202
+// Добавьте другие идентификаторы здесь
